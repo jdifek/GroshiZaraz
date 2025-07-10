b0174b5c25b59fc69d875f65b6b371f1cd4f0034
jdifek <givotkov11@gmail.com>
2025-07-10 15:14:12 +0200

    home first

--- next.config.ts
+++ next.config.ts
@@ -12,18 +12,7 @@ const nextConfig = {
       "mfo.qissseee.tech"
     ],
   },
-  swcMinify: false,
-  // Отключаем lightningcss
-  compiler: {
-    // Включаем поддержку tailwindcss и отключаем lightningcss
-    styledComponents: false,
-    // Отключить lightningcss (Next.js >=15)
-    emotion: false,
-    // Важно:
-    // useLightningCss: false
-    useLightningCss: false,
-  },
-
+  
 };
 
 module.exports = withNextIntl(nextConfig);
