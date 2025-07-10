ee66f22dddacf76473c62f148a98a1d49c5bac54
jdifek <givotkov11@gmail.com>
2025-07-10 15:11:14 +0200

    home first

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,16 @@ const nextConfig = {
     ],
   },
   swcMinify: false,
+  // Отключаем lightningcss
+  compiler: {
+    // Включаем поддержку tailwindcss и отключаем lightningcss
+    styledComponents: false,
+    // Отключить lightningcss (Next.js >=15)
+    emotion: false,
+    // Важно:
+    // useLightningCss: false
+    useLightningCss: false,
+  },
 
 };
 
