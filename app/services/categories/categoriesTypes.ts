76d30d328f7c486c2dad4002f547db2fcf7abd6c
jdifek <givotkov11@gmail.com>
2025-08-05 19:31:41 +0200

    wqwq

---
+++ app/services/categories/categoriesTypes.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  icon?: string;
+  nameUk: string;
+}
+export interface CategoryCreateDto {
+  name: string;
+  icon?: string;
+  nameUk: string;
+}
+
+export interface CategoryUpdateDto extends CategoryCreateDto {}
