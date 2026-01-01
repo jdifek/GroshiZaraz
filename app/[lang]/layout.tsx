3c28231759bd607e6332bd4f3aab55a35f86e1fe
jdifek <givotkov11@gmail.com>
2026-01-01 18:12:39 +0200

    new fix

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -28,7 +28,7 @@ export async function generateMetadata({
       keywords: messages.Metadata.root.keywords,
       robots: "index, follow",
       icons: {
-        icon: "/logoNew.ico", // ✅ фавикон для всех страниц
+        icon: "/favicon.ico", // ✅ фавикон для всех страниц
       },
       openGraph: {
         title: messages.Metadata.root.title,
